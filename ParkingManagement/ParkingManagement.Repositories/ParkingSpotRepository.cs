7efbd25de9f51bd2da72061ce2ca3a002a6fe37f
Marija Aleksovska <marijaaleksovska7@gmail.com>
2020-12-10 19:30:42 +0100

    Add files via upload

---
+++ ParkingManagement/ParkingManagement.Repositories/ParkingSpotRepository.cs
@@ -0,0 +1,32 @@
+﻿using ParkingManagement.Models;
+using System;
+
+namespace ParkingManagement.Repositories
+{
+    public class ParkingSpotRepository : BaseRepository<ParkingSpot>
+    {
+        protected override string GetFileName()
+        {
+            return "ParkingSpots.txt";
+        }
+
+        public void Seed()
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                var current = new ParkingSpot();
+                current.Id = i;
+
+                if(i % 2 ==0)
+                {
+                    current.CarRegistrationNumber = $"asd{i}";
+                    current.ParkingStarted = DateTime.Now;
+                }
+
+                Add(current);
+            }
+
+            SaveChanges();
+        }
+    }
+}
---
+++ ParkingManagement/ParkingManagement.Repositories/PrepaidTicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using ParkingManagement.Models;
+
+namespace ParkingManagement.Repositories
+{
+    public class PrepaidTicketRepository : BaseRepository<PrepaidTicket>
+    {
+        protected override string GetFileName()
+        {
+            return "PrepaidTickets.txt";
+        }
+    }
+}
