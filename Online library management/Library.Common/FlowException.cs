9f85a0c5d0994b07e2d6e3c10c9e8d1593c4f1bd
Marija Aleksovska <marijaaleksovska7@gmail.com>
2020-12-10 19:47:17 +0100

    Add files via upload

---
+++ Online library management/Library.Common/FlowException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Common
+{
+    public class FlowException:Exception
+    {
+        public FlowException(string msg):base(msg)
+        {
+
+        }
+    }
+}
